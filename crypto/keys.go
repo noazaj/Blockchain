f3d12c68e202a3d185dd04b30e0c97497319ff25
Noah Zajicek <nzajicek@WVWAUTHORITY.COM>
2024-03-19 13:08:04 -0400

    First commit

---
+++ crypto/keys.go
@@ -0,0 +1,19 @@
+package crypto
+
+import "crypto/ed25519"
+
+type PrivateKey struct {
+	key ed25519.PrivateKey
+}
+
+func (p *PrivateKey) Bytes() []byte {
+	return p.key
+}
+
+func (p *PrivateKey) Sign(msg []byte) []byte {
+	return ed25519.Sign(p.key, msg)
+}
+
+type PublicKey struct {
+	key ed25519.PublicKey
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/noazaj/blockchain
+
+go 1.22.0
